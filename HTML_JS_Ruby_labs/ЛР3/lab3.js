47d0ec93c350338682cb097cb2ed9fc864043c5a
Daria Katkova <d.s.katkova@ya.ru>
2023-12-11 18:43:32 +0300

    Добавлены ЛР1-3

---
+++ HTML_JS_Ruby_labs/ЛР3/lab3.js
@@ -0,0 +1,30 @@
+let elTree = "";
+let count = 0;
+
+function buttonClick() {
+    let nodeList = document.getElementById("html").children;
+    elTree += "<!DOCTYPE html>\n<html>\n";
+    Output(nodeList);
+    elTree += "\n</html>";
+    console.log(elTree);
+    let text = document.createTextNode(elTree);
+    document.getElementById("htmlCode").appendChild(text);
+}
+
+function Output(nodes) {
+    count += 1;
+    for (let i = 0; i < nodes.length; i++) {
+        if (i != 0) elTree += "\n"
+        for (let j = 0; j < count; j++) elTree += "\u0009";
+            if (nodes[i].localName != "br") elTree += "<" + nodes[i].localName + ">";
+        if (nodes[i].children.length != 0) {
+            elTree += "\n"
+            Output(nodes[i].children)
+            elTree += "\n";
+            count -= 1;
+            for (let j = 0; j < count; j++) elTree += "\u0009";
+            elTree += "</" + nodes[i].localName + ">";   
+        }
+        else elTree += "</" + nodes[i].localName + ">";
+    }
+}
